226c3596077f247821a0580e5c6460448dd6c015
mvarnagiris <mantas.varnagiris@gmail.com>
2013-02-08 15:13:56 +0000

    Updated how BorderImageProcessor draws shadows

--- ImageLoader/src/com/code44/imageloader/processor/BorderImageProcessor.java
+++ ImageLoader/src/com/code44/imageloader/processor/BorderImageProcessor.java
@@ -11,8 +11,8 @@ import android.graphics.RectF;
 public class BorderImageProcessor implements ImageProcessor
 {
 	protected final Paint	borderPaint		= new Paint();
-	protected final Paint	borderMaskPaint	= new Paint();
 	protected final Paint	bitmapMaskPaint	= new Paint();
+	protected final Paint	bitmapPaint		= new Paint();
 
 	protected final int		boderColor;
 	protected final int		borderWidth;
@@ -38,18 +38,18 @@ public class BorderImageProcessor implements ImageProcessor
 				+ shadowDX + "_" + shadowDY + "_" + shadowColor;
 
 		this.borderPaint.setAntiAlias(true);
-		this.borderPaint.setColor(borderColor);
+		this.borderPaint.setColor(boderWidth <= 0 ? shadowColor : borderColor);
 		if (shadowRadius > 0)
 			this.borderPaint.setShadowLayer(shadowRadius, shadowDX, shadowDY, shadowColor);
 
-		this.borderMaskPaint.setAntiAlias(true);
-		this.borderMaskPaint.setColor(borderColor);
-		this.borderMaskPaint.setXfermode(new PorterDuffXfermode(Mode.CLEAR));
-
 		this.bitmapMaskPaint.setAntiAlias(true);
 		this.bitmapMaskPaint.setColor(borderColor);
-		this.bitmapMaskPaint.setFilterBitmap(true);
-		this.bitmapMaskPaint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+		// this.bitmapMaskPaint.setXfermode(new PorterDuffXfermode(Mode.CLEAR));
+
+		this.bitmapPaint.setAntiAlias(true);
+		this.bitmapPaint.setColor(borderColor);
+		this.bitmapPaint.setFilterBitmap(true);
+		this.bitmapPaint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
 	}
 
 	@Override
@@ -83,28 +83,20 @@ public class BorderImageProcessor implements ImageProcessor
 		final RectF bitmapRectDst = new RectF(bitmapL, bitmapT, bitmapR, bitmapB);
 		final RectF borderRect = new RectF(bitmapL - borderWidth, bitmapT - borderWidth, bitmapR + borderWidth, bitmapB + borderWidth);
 
+		// Create rounded scaled bitmap
 		Canvas canvas;
-		Bitmap border = null;
-		if (borderWidth > 0)
-		{
-			border = Bitmap.createBitmap(bWidth, bHeight, Config.ARGB_8888);
-			canvas = new Canvas(border);
-
-			canvas.drawRoundRect(borderRect, borderRadius + borderWidth / 2, borderRadius + borderWidth / 2, borderPaint);
-			canvas.drawRoundRect(bitmapRectDst, borderRadius, borderRadius, borderMaskPaint);
-		}
-
-		final Bitmap output = Bitmap.createBitmap(bWidth, bHeight, Config.ARGB_8888);
-		canvas = new Canvas(output);
-
-		canvas.drawRoundRect(bitmapRectDst, borderRadius, borderRadius, borderPaint);
-		canvas.drawBitmap(bitmap, null, bitmapRectDst, bitmapMaskPaint);
-		if (border != null)
-		{
-			canvas.drawBitmap(border, null, borderRect, null);
-			border.recycle();
-		}
+		final Bitmap roundedScaledBitmap = Bitmap.createBitmap(bWidth, bHeight, Config.ARGB_8888);
+		canvas = new Canvas(roundedScaledBitmap);
+		canvas.drawRoundRect(bitmapRectDst, borderRadius, borderRadius, bitmapMaskPaint);
+		canvas.drawBitmap(bitmap, null, bitmapRectDst, bitmapPaint);
 		bitmap.recycle();
+
+		// Draw border and shadow, then draw rounded scaled bitmap on top
+		Bitmap output = null;
+		output = Bitmap.createBitmap(bWidth, bHeight, Config.ARGB_8888);
+		canvas = new Canvas(output);
+		canvas.drawRoundRect(borderRect, borderRadius + borderWidth / 2, borderRadius + borderWidth / 2, borderPaint);
+		canvas.drawBitmap(roundedScaledBitmap, null, bitmapRectDst, null);
 
 		return output;
 	}
